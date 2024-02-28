54ed86841e9a2b90c01d7886218134103784e975
rossmoody <fiftyfivehis@gmail.com>
2024-02-28 09:25:45 -0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ export function Hero() {
         <Button href="https://chromewebstore.google.com/detail/svg-gobbler/mpbmflcodadhgafbbakjeahpandgcbch">
           Install SVG Gobbler
         </Button>
-        <Button href="https://youtu.be/0GtqUqTgUOg" variant="outline">
+        <Button href="https://youtu.be/0GtqUqTgUOg" target="_blank" variant="outline">
           <svg
             aria-hidden="true"
             className="h-3 w-3 flex-none fill-red-600 group-active:fill-current"
