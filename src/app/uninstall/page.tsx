818d5379d3f075459f7deba5efbed6fb0a757180
rossmoody <fiftyfivehis@gmail.com>
2024-05-27 20:52:06 -0700

    update

--- src/app/uninstall/page.tsx
+++ src/app/uninstall/page.tsx
@@ -1,17 +1,8 @@
-'use client'
-
 import { Button } from '@/components/Button'
 import { Logo } from '@/components/Logo'
 import Link from 'next/link'
-import { useState } from 'react'
 
 export default function UninstallPage() {
-  const [submitted, setSubmitted] = useState(false)
-
-  const handleSubmit = () => {
-    setSubmitted(true)
-  }
-
   return (
     <div>
       <div className="flex">
@@ -19,32 +10,30 @@ export default function UninstallPage() {
           <Logo className="h-7 w-auto" />
         </Link>
       </div>
-      {submitted ? (
-        <h1 className="mt-12 text-xl font-semibold text-gray-900">Thank you for your feedback!</h1>
-      ) : (
-        <>
-          <h1 className="mt-12 text-xl font-semibold text-gray-900">
-            Real quick, why did you uninstall?
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            Your feedback is important and it&apos;s critical we understand how SVG Gobbler
-            didn&apos;t meet your needs. <i>Include your email if you want us to follow up.</i>
-          </p>
-
-          <form data-netlify="true" method="POST" name="uninstall" netlify-honeypot="bot-field">
-            <input name="form-name" type="hidden" value="uninstall" />
-            <textarea
-              autoFocus
-              className="mt-6 h-32 w-full rounded-md border border-gray-300 p-4 text-sm"
-              name="feedback"
-              placeholder="The extension wasn't able to...."
-            />
-            <Button className="mt-4 w-full" color="red" onClick={handleSubmit} type="submit">
-              Submit feedback
-            </Button>
-          </form>
-        </>
-      )}
+      <h1 className="mt-12 text-xl font-semibold text-gray-900">
+        Real quick, why did you uninstall?
+      </h1>
+      <p className="mt-2 text-sm text-gray-700">
+        Your feedback is important and it&apos;s critical we understand how SVG Gobbler didn&apos;t
+        meet your needs. <i>Include your email if you want us to follow up.</i>
+      </p>
+      <form
+        action="/"
+        data-netlify="true"
+        method="POST"
+        name="uninstall"
+        netlify-honeypot="bot-field"
+      >
+        <textarea
+          autoFocus
+          className="mt-6 h-32 w-full rounded-md border border-gray-300 p-4 text-sm"
+          name="feedback"
+          placeholder="The extension wasn't able to...."
+        />
+        <Button className="mt-4 w-full" color="red" type="submit">
+          Submit feedback
+        </Button>
+      </form>
     </div>
   )
 }
