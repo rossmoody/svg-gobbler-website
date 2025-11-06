0fbbd4b599f6d20dbf494600a0f39e48e8c93325
Ross Moody <fiftyfivehis@gmail.com>
2025-11-05 17:00:39 -0800

    update uninstall page

--- src/app/uninstall/page.tsx
+++ src/app/uninstall/page.tsx
@@ -11,11 +11,12 @@ export default function UninstallPage() {
           <Logo className="h-7 w-auto" />
         </Link>
       </div>
-      <h1 className="mt-12 text-xl font-semibold text-gray-900">
-        Why didn&apos;t SVG Gobbler work for you?
+      <h1 className="mt-12 text-2xl font-semibold text-gray-900">
+        Are you super angry because SVG Gobbler sucks?
       </h1>
-      <p className="mt-2 text-sm text-gray-700">
-        Help make the extension better by sharing your feedback.
+      <p className="mt-2 text-gray-700">
+        Please let us know how bad it sucks and especially the site where it failed to meet your
+        expectations.
       </p>
       <form
         action="/thank-you"
@@ -27,7 +28,7 @@ export default function UninstallPage() {
         <textarea
           autoFocus
           className="mb-2 mt-6 h-32 w-full rounded-md border border-gray-300 p-4 text-sm"
-          minLength={25}
+          minLength={20}
           name="feedback"
           placeholder="The extension wasn't able to...."
           required
