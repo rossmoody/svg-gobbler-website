2b085bb3664d49f48efe7ddabd28686e0cbaaeaf
Ross Moody <fiftyfivehis@gmail.com>
2025-11-28 20:11:02 -0800

    Update route.ts

--- src/app/api/get-counter/route.ts
+++ src/app/api/get-counter/route.ts
@@ -3,8 +3,13 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   try {
     const response = await fetch(
-      `https://storage.googleapis.com/svg-gobbler/counter/counter.json?v=${Date.now()}`,
-      { cache: 'no-store' },
+      `https://storage.googleapis.com/svg-gobbler/counter/counter.json`,
+      {
+        cache: 'no-store',
+        headers: {
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+        },
+      },
     )
     const data = await response.json()
     return NextResponse.json({ data }, { status: 200 })
