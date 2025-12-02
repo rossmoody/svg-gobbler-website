0bb94df56956314eb241cd7a8989340e57b96be3
Ross Moody <fiftyfivehis@gmail.com>
2025-12-02 15:10:28 -0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+        source: '/api/get-counter',
+      },
+    ]
+  },
   images: { unoptimized: true },
   output: 'export',
 }
--- src/app/api/get-counter/route.ts
+++ src/app/api/get-counter/route.ts
@@ -2,11 +2,9 @@ import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
-    const response = await fetch(
-      `https://storage.googleapis.com/svg-gobbler/counter/counter.json?v=${Date.now()}`,
-    )
+    const response = await fetch('https://storage.googleapis.com/svg-gobbler/counter/counter.json')
     const data = await response.json()
     return NextResponse.json({ data }, { status: 200 })
   } catch (error) {
