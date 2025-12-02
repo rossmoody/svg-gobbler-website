f7779d1cee4d8b71a4cac84c1fad59369e75472e
Ross Moody <fiftyfivehis@gmail.com>
2025-12-02 15:43:06 -0800

    Update route.ts

--- src/app/api/get-counter/route.ts
+++ src/app/api/get-counter/route.ts
@@ -2,12 +2,18 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   try {
-    const response = await fetch(
-      'https://storage.googleapis.com/svg-gobbler/counter/counter.json',
-      { cache: 'no-store' }, // This prevents caching
-    )
+    const response = await fetch('https://storage.googleapis.com/svg-gobbler/counter/counter.json')
     const data = await response.json()
-    return NextResponse.json({ data }, { status: 200 })
+
+    return NextResponse.json(
+      { data },
+      {
+        headers: {
+          'Cache-Control': 'no-store, max-age=0',
+        },
+        status: 200,
+      },
+    )
   } catch (error) {
     console.error('Error fetching counter:', error)
     return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 })
