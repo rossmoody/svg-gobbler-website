2526ff55c01db1a1362d364c0cfd71136a835e51
Ross Moody <fiftyfivehis@gmail.com>
2025-12-02 15:15:15 -0800

    update

--- src/app/api/get-counter/route.ts
+++ src/app/api/get-counter/route.ts
@@ -4,7 +4,12 @@ export const revalidate = 0
 
 export async function GET() {
   try {
-    const response = await fetch('https://storage.googleapis.com/svg-gobbler/counter/counter.json')
+    const response = await fetch(
+      'https://storage.googleapis.com/svg-gobbler/counter/counter.json',
+      {
+        cache: 'no-store',
+      },
+    )
     const data = await response.json()
     return NextResponse.json({ data }, { status: 200 })
   } catch (error) {
