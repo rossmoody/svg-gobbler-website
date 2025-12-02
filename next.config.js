147242dc9665f7955754b475a9ace97ab7a35320
Ross Moody <fiftyfivehis@gmail.com>
2025-12-02 15:15:57 -0800

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-        source: '/api/get-counter',
-      },
-    ]
-  },
   images: { unoptimized: true },
   output: 'export',
 }
