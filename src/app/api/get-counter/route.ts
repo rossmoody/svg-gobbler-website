688060e76498d40ca4be27d422ee877ba3b721e7
Ross Moody <fiftyfivehis@gmail.com>
2025-12-02 15:18:25 -0800

    Update route.ts

--- src/app/api/get-counter/route.ts
+++ src/app/api/get-counter/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export const revalidate = 0
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
     const response = await fetch(
       'https://storage.googleapis.com/svg-gobbler/counter/counter.json',
